080b406957a00df904c9b766bef383ed8c2e661a
Sebastian Qu <sebastianqu@gmail.com>
2016-10-28 17:46:55 +0800

    add project

---
+++ config.js
@@ -0,0 +1,39 @@
+// all environment config
+let config = {
+    server: {
+        serviceRoot: 'api',
+        port: 30001
+    }
+};
+
+// development environment
+const devEnv = {
+    database : {
+        url: 'localhost',
+        port: 27017,
+        user: 'test',
+        passwd: 'test@1123',
+        schema: 'test',
+        poolSize: 10
+    }
+};
+
+// producation enviroment
+const prodEnv = {
+    database : {
+        url: '192.168.11.23',
+        port: 27017,
+        user: 'test',
+        passwd: 'test@1123',
+        schema: 'test',
+        poolSize: 10
+    }
+};
+
+if (process.env.NODE_ENV === 'producation') {
+    config = Object.assign(config, prodEnv);
+} else {
+    config = Object.assign(config, devEnv);
+}
+
+module.exports = config;
